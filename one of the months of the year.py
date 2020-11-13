e7ecf906601c2f0e0faee6ada0d3ae95ff6f6a54
chidieberex <60482344+chidieberex@users.noreply.github.com>
2020-11-13 10:15:59 -0600

    Update one of the months of the year.py
    #also having difficulty saving this file. just giving it another try.

--- one of the months of the year.py
+++ one of the months of the year.py
@@ -5,3 +5,5 @@ months = ["January", "February", "March", "April", "May", "June",
 for c in months:
     sentence = "One of the months of the year is " + c
     print(sentence)
+
+#also having difficulty saving this file. just giving it another try.
